cadb4a47081cf276d2c37a35fbbb14e65ed33032
bsaoudi <bilel.seoudi@gmail.com>
2022-04-04 11:31:38 +0200

    Logging Aspect

---
+++ src/main/java/com/afklm/usermanagement/logging/JsonUtils.java
@@ -0,0 +1,31 @@
+package com.afklm.usermanagement.logging;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+/**
+ * This is a JSON utility class that should contain only static helper methods.
+ */
+public final class JsonUtils {
+
+    private JsonUtils() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    public static byte[] toJson(Object o) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper.writeValueAsBytes(o);
+    }
+
+    public static String toJsonString(Object o) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper.writeValueAsString(o);
+    }
+
+}
